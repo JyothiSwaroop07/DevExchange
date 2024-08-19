4f8c4eb7e7a3b8502c279232f0703ec7578988bd
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-08-19 20:07:57 +0530

    next js environment setup

---
+++ devexchange-app/devexchange-client/src/app/Home/page.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Home = () => {
+    return (
+        <>
+            <h1 className="text-xl">This is Home page</h1>
+        </>
+    )
+}
+
+export default Home;
---
+++ devexchange-app/devexchange-client/src/app/Profile/page.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Profile = () =>{
+    return (
+        <>
+            <h1>This is Profile Page</h1>
+        </>
+    )
+}
+
+export default Profile;
