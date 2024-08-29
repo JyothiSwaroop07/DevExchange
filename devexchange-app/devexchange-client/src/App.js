737e033c206e333a25a2bd42ee9e64ffa53ec210
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-08-29 12:13:02 +0530

    ask question navigation

--- devexchange-app/devexchange-client/src/App.js
+++ devexchange-app/devexchange-client/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Routing from './Routing';
+import AskQuestion from './components/AskQuestion/AskQuestion';
 
 function App() {
   return (
