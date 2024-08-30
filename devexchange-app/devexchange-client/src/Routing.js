674da171025a2402f654fa037a332147376445bd
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-08-30 12:36:53 +0530

    main bar created with sample questions

--- devexchange-app/devexchange-client/src/Routing.js
+++ devexchange-app/devexchange-client/src/Routing.js
@@ -5,6 +5,7 @@ import { Routes, Route} from 'react-router-dom';
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 import AskQuestion from "./components/AskQuestion/AskQuestion";
+import Questions from "./components/Questions/Questions";
 
 const Routing = () => {
     return (
@@ -12,6 +13,7 @@ const Routing = () => {
             <Route exact path="/" Component={Home} />
             <Route exact path="/Auth" Component={Auth} />
             <Route exact path="/AskQuestion" Component={AskQuestion} />
+            <Route exact path="/Questions" Component={Questions} />
         </Routes>
     )
 }
