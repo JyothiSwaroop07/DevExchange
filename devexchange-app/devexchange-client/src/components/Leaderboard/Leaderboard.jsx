ad18225c948cd770a1de28056b49d05c5c89ed0b
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-10-08 20:40:03 +0530

    Leaderboard component added

--- devexchange-app/devexchange-client/src/components/Leaderboard/Leaderboard.jsx
+++ devexchange-app/devexchange-client/src/components/Leaderboard/Leaderboard.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import LeftSidebar from "../LeftSidebar/LeftSidebar";
+
+import '../../App.css';
 
 const LeaderBoard = () => {
     return (
-        <div>
-            Leaderboard
+        <div className="home-container1">
+            <LeftSidebar />
+            <div className="home-container2">
+
+            </div>
         </div>
     )
 }
-
 export default LeaderBoard;
