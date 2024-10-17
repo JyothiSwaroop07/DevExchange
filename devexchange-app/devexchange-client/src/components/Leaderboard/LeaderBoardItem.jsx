9d2932a99e1d25d03932bb56d5e2e6a88164618b
jyothiswaroop07 <vv5382@srmist.edu.in>
2024-10-17 15:26:01 +0530

    leaderboard page added

--- devexchange-app/devexchange-client/src/components/Leaderboard/LeaderBoardItem.jsx
+++ devexchange-app/devexchange-client/src/components/Leaderboard/LeaderBoardItem.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './Leaderboard.css'
 
-const LeaderBoardItem = ({item}) => {
-    
+const LeaderboardItem = ({user, index}) => {
+    console.log(user);
+
+    const color = index%2===0 ? 'aliceblue' : 'antiquewhite';
+
+    return (
+        <Link to={`/Users/${user._id}`} className={`user-item ${color}`}>
+            <h5>
+                {index}
+            </h5>
+            <h5>
+                {user.name}
+            </h5>
+            <h5>
+                {user.email}
+            </h5>
+        </Link>
+    )
 }
+
+export default LeaderboardItem;
--- devexchange-app/devexchange-client/src/components/Leaderboard/Leaderboard.jsx
+++ devexchange-app/devexchange-client/src/components/Leaderboard/Leaderboard.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import LeftSidebar from "../LeftSidebar/LeftSidebar";
+import { useLocation } from "react-router-dom";
 
 import '../../App.css';
+import '../Users/Users.css'
+
+import LeaderboardList from "./LeaderboardList";
 
 const LeaderBoard = () => {
-    return (
-        <div className="home-container1">
-            <LeftSidebar />
-            <div className="home-container2">
 
+    const location = useLocation();
+
+    return (
+        <div className="users-page">
+            <LeftSidebar />
+            <div className="user-section">
+            {
+                    location.pathname==='/leaderboard' ? 
+                    
+                    <LeaderboardList /> :
+                    <></>
+                }
             </div>
         </div>
     )
